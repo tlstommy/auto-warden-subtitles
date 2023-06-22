a3565003810cf00dc062015fffc96a1c1d7001f4
lulamae12 <tls.tommy11@gmail.com>
2023-06-22 13:39:01 -0400

    working!

--- src/client/java/tls/autowardensubtitles/ExampleModClient.java
+++ src/client/java/tls/autowardensubtitles/ExampleModClient.java
@@ -2,11 +2,12 @@ package tls.autowardensubtitles;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.registry.RegistryKey;
-import net.minecraft.util.Identifier;
-import net.minecraft.registry.Registries;
+import net.minecraft.text.Text;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeKeys;
+import net.minecraft.client.option.GameOptions;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,19 +16,46 @@ public class ExampleModClient implements ClientModInitializer {
 
 	private static final Logger LOGGER = LogManager.getLogger();
 
+	private boolean subtitlesEnabled = false;
+
 	@Override
 	public void onInitializeClient() {
 
-		RegistryKey<Biome> targetBiome = BiomeKeys.RIVER;
+
+		//set the target biome to deep dark
+		RegistryKey<Biome> targetBiome = BiomeKeys.DEEP_DARK;
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			if (client.player != null && client.world != null) {
 				Biome biome = client.world.getBiome(client.player.getBlockPos()).value();
 				Boolean inTargetBiome = client.world.getBiome(client.player.getBlockPos()).matchesKey(targetBiome);
-				LOGGER.info("In biome [RIVER]: "+inTargetBiome);
 
+				if(inTargetBiome && !subtitlesEnabled) {
+					LOGGER.info("Turn subtitles on");
+					subtitlesEnabled = true;
+					client.player.sendMessage(Text.literal("Now entering The Deep Dark, turning on subtitles"));
+					turnOnSubtitles();
 
+				}
+
+				if(!inTargetBiome && subtitlesEnabled) {
+					LOGGER.info("Turn subtitles off");
+					client.player.sendMessage(Text.literal("Leaving The Deep Dark, turning off subtitles"));
+					subtitlesEnabled = false;
+					turnOffSubtitles();
+				}
 			}
 		});
+	}
+
+	private void turnOnSubtitles(){
+		GameOptions gameOptions = MinecraftClient.getInstance().options;
+		gameOptions.getShowSubtitles().setValue(true);
+	}
+
+	private void turnOffSubtitles(){
+		GameOptions gameOptions = MinecraftClient.getInstance().options;
+		gameOptions.getShowSubtitles().setValue(false);
+
 	}
 }
